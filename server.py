d9aa6cb2d1729d50ba5fc4bc80922c2b46ba05b0
Lucian Cibu <luciancibu@yahoo.com>
2025-11-15 22:31:16 +0200

    Add white list

--- server.py
+++ server.py
@@ -12,11 +12,15 @@ def home():
 
 @app.route("/run", methods=["GET","POST"])
 def run_selenium():
+    client_ip = request.remote_addr
+    if client_ip not in IP_WHITELIST:
+        return f"Forbidden: IP {client_ip} not allowed", 403
+
     auth_header = request.headers.get("Authorization")
     if not auth_header or not auth_header.startswith("Bearer "):
         return "Unauthorized: Missing or invalid Authorization header", 401
     token = auth_header.split(" ")[1] # Get token
-    if token != SECRET_TOKEN:
+    if token not in TOKEN_WHITELIST:
         return "Unauthorized: Invalid token", 401    
 
     day = request.args.get("day")
